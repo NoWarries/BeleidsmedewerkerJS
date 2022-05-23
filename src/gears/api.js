36ba16d6917f4bb619f9ffd99b5b902bc67e855d
Coen Warries <coen.warries@student.hu.nl>
2022-05-23 21:54:04 +0200

    add extra check to user api

--- src/gears/api.js
+++ src/gears/api.js
@@ -18,6 +18,9 @@ app.get("/v1/user/:userID", (req, res) => {
     db.getUser(req.params.userID).then(data => {
         db.getLevelTable().then(dataTable => { 
 
+            if(data === null) {
+                res.status(404).json({ error: "User ID invalid" });
+            }
             // Calculate togo xp for next level and add it to the progress array
             data.progress.togo = dataTable[data.progress.level]["xp"] - data.progress.xp || 0;
 
