6de5b377f18e590d7cd5cd8e3a723e313112a7ea
Coen Warries <coen.warries@student.hu.nl>
2022-05-23 15:14:06 +0200

    change port to match with application

--- src/gears/api.js
+++ src/gears/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 28883;
 
 app.listen(port, () => {
     console.log(`[ 🔧 ] API listening to port : ${port}`);
