a1af8c6d6bb3cafdb50cd96bce0483355ebe0569
Coen Warries <coen.warries@student.hu.nl>
2022-05-26 21:15:53 +0200

    cleaning up + fix with client import

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,34 +2,37 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import moment from "moment";
 import "moment-duration-format";
 import { MessageEmbed } from "discord.js";
-import { client } from "../main.js";
 import * as config from "../../config/common.js";
+
 
 let data = new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Send a ping to the bot");
 
-async function execute(interaction) {
-    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+function execute(interaction) {
+    import("../main.js").then( client => { 
+        (async () => {
+            const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+            console.log(duration);
+            const originalEmbed = new MessageEmbed()
+                .setTimestamp()
+                .setTitle(`${config.info.shorthand} - Ping`)
+                .setColor(config.colors.clrMain)
+                .setThumbnail(interaction.guild.iconURL())
+                .setDescription("Pinging...");
 
-    const originalEmbed = new MessageEmbed()
-        .setTimestamp()
-        .setTitle(`${config.info.shorthand} - Ping`)
-        .setColor(config.colors.clrMain)
-        .setThumbnail(interaction.guild.iconURL())
-        .setDescription("Pinging...");
+            const newEmbed = new MessageEmbed()
+                .setTimestamp()
+                .setTitle(`${config.info.shorthand} - Ping`)
+                .setColor(config.colors.clrMain)
+                .setThumbnail(interaction.guild.iconURL())
+                .addField("⏱️ Ping data", "Time " + Math.round(Date.now() - interaction.createdTimestamp) + "ms \n Heartbeat: " + Math.round(client.ws.ping) + "ms")
+                .addField("👁️‍🗨️ Uptime", duration);
 
-    const newEmbed = new MessageEmbed()
-        .setTimestamp()
-        .setTitle(`${config.info.shorthand} - Ping`)
-        .setColor(config.colors.clrMain)
-        .setThumbnail(interaction.guild.iconURL())
-        .addField("⏱️ Ping data", "Time " + Math.round(Date.now() - interaction.createdTimestamp) + "ms \n Heartbeat: " + Math.round(client.ws.ping) + "ms")
-        .addField("👁️‍🗨️ Uptime", duration);
-
-    let fulfilledEmbed = await interaction.channel.send({ embeds: [originalEmbed] });
-    fulfilledEmbed.edit({ embeds: [newEmbed] });
-
+            let fulfilledEmbed = await interaction.channel.send({ embeds: [originalEmbed] });
+            fulfilledEmbed.edit({ embeds: [newEmbed] });
+        })();
+    });
 }
 
 
