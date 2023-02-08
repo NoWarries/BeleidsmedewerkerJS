88ed3645a99b0e6ea7db9cd0eb4c452ca7eb1d07
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2023-02-08 13:51:50 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     "env": {
         "node": true,
         "es2021": true
-    },
+    }, 
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": "latest",
