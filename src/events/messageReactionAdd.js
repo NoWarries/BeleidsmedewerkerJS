439211eccd3948bda341c2e2d9e090c005724ce7
Coen Warries <coen.warries@student.hu.nl>
2023-07-27 16:41:55 +0200

    fix: hotfix existing emojis

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -15,7 +15,7 @@ async function execute(interaction)
     const guildConfig = db.getTable("server", interaction.message.guildId) || null;
     const {client} = await import("../main.js");
 
-    guildConfig.then(data => {
+    guildConfig.then(async data => {
         /*
         If server has config
         */
@@ -25,9 +25,32 @@ async function execute(interaction)
             */
             if (data.voteChannel === interaction.message.channelId || data.councilVoteChannel === interaction.channel){
                 // If not an upvote or downvote
-                if(interaction.emoji.id !== guild.emoji.upvote && interaction.emoji.id !== guild.emoji.downvote) {
+
+                // fetch the message
+                const reaction = await interaction.message.channel.messages.fetch(interaction.message.id).then((message) => {
+                    // fetch the reaction
+                    return message.reactions.cache.find(
+                        (reaction) => {
+                            return reaction.emoji.name === interaction.emoji.name || (reaction.emoji.id != null && reaction.emoji.id === interaction.emoji.id);
+                        }
+                    );
+                
+                });
+
+                const botAuthored = await reaction.users.fetch().then((users) => {
+                    return users.has(client.user.id);
+                });
+
+                // Reaction is not an upvote or downvote and bot did not add the reaction
+                if(interaction.emoji.id !== guild.emoji.upvote && interaction.emoji.id !== guild.emoji.downvote && !botAuthored) {
+
                     // remove reaction
-                    interaction.emoji.reaction.remove();
+                    if(interaction.emoji.id !== null) {
+                        interaction.message.reactions.cache.get(interaction.emoji.id).remove();
+                    } else {
+                        interaction.message.reactions.cache.get(interaction.emoji.name).remove();
+                    }
+
                     // send message and remove after 5 seconds
                     interaction.message.channel.send({
                         embeds: [
