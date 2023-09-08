68d945258778fdb1894a414e0240a0252d68582e
Coen Warries <coen.warries@student.hu.nl>
2023-09-08 22:46:10 +0200

    feat: simple good morning command / cron-job

---
+++ src/lib/morningRoutine/morning_hi_messages.js
@@ -0,0 +1,7 @@
+export default [
+    "Heey schatje :yellow_heart: goedemorgen!",
+    "Goedemorgen kameraad! :person_shrugging: ",
+    "Goedemorgen, ik hoop dat je goed geslapen hebt! :sleeping:",
+    "Het is 9 uur, tijd voor zelfpijni... euh, ik bedoel, tijd om op te staan! :smirk:",
+    ":wave: Goedemorgen!",
+];
