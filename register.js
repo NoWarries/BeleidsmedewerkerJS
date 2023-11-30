1f8d800a0ca82de62f9251325bfdfc474bf930c0
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2023-11-30 12:39:48 +0100

    fix: refactor command registration process

--- register.js
+++ register.js
@@ -1,25 +1,32 @@
 import "dotenv/config";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import {extractFilesRecursively} from "./src/handlers/reader.js";
+import { extractFilesRecursively } from "./src/handlers/reader.js";
 
-const commands = [];
-const commandFiles = await extractFilesRecursively("./src/commands");
-(async () => {
+const registerCommands = async () => {
+    const commands = [];
+    const commandFiles = await extractFilesRecursively("./src/commands");
+
     for (const file of commandFiles) {
         const { data } = await import(`./${file}`);
         console.log(`[ ✍️  ] Registering : ${file}`);
         commands.push(data.toJSON());
     }
-})().then( () => {
+
+    return commands;
+};
+
+const registerCommandsInDiscord = async () => {
+    const commands = await registerCommands();
+
     const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
-    (async () => {
-        try {
-            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-                .then(() => console.log("✔️  => Commands registered"))
-                .catch(console.error);
-        } catch (error) {
-            console.error(error);
-        }
-    })();
-});
+
+    try {
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+        console.log("✔️  => Commands registered");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+registerCommandsInDiscord();
